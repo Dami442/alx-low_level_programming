9315f5652835b5792c58247a107f0a1c7f2f05a9
Dami442 <damolowe442@gmail.com>
2023-03-17 11:34:39 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,10 @@
 /**
  *main - print out a statement using puts
  *
- * return - 0
+ * return - print successfully
 */
- 
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
