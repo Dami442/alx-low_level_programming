fbaab402a95c93e47ff6572dd5a81134515e2bd7
Dami442 <damolowe442@gmail.com>
2023-03-17 11:47:14 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - prints out statement using printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with properr grammar, but the outcome is a piece of art\n");
+	return (0);
+}
