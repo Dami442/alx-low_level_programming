1a9073cea312d2507f38528f5bf3f584902d8498
Dami442 <damolowe442@gmail.com>
2023-03-22 21:11:43 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints out small letters ten times
+ *
+ * Return: 0 Always
+ */
+
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+
+		_putchar(j);
+	_putchar('\n');
+	}
+}
+
