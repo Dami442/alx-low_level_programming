110ad2209b0f982646007ab38d01836c69935e0f
Dami442 <damolowe442@gmail.com>
2023-03-27 16:35:47 -0700

    2-str

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ *@s: parameter to be used
+ *
+ *Return: 0
+ */
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	while (*s != 0)
+	{
+		length++;
+		s++;
+	}
+	return (0);
+}
