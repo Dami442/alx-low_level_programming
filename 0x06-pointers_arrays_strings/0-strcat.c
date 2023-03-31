75f36779013cb8eb4cac811263e5dc0e43f2279d
Dami442 <damolowe442@gmail.com>
2023-03-31 10:27:59 -0700

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,26 +7,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i =0;
-	int j = 0;
+	int i;
+	int j;
 
-	char *str1;
-	char *str2;
-
-	str1 = dest;
-	str2 = src;
-
+	i = 0;
 	while (dest[i] != '\0')
 	{
-		++str1;
 		i++;
 	}
-	while (src[j] != '\0')
+	for (j = 0; src[j] != '\0'; ++j, ++i)
 	{
-		*str1 = *str2;
-		str1++;
-		str2++;
-		j++;
+		dest[i] = src[j];
 	}
+	dest[i] = '\0';
 	return (0);
 }
