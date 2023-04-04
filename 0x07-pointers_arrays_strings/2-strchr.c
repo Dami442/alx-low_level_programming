7dd2934fcd2e2f5649ab242d608271d6080782f3
Dami442 <damolowe442@gmail.com>
2023-04-03 17:32:48 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: parameter for string
+ * @c: character parameter
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+
+		}
+		else
+		{
+			s++;
+		}
+
+	}
+	return (NULL);
+}
