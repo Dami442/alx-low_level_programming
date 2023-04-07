52f728a6bc31ace3d22ad9346c3a0d04d7094a59
Dami442 <damolowe442@gmail.com>
2023-04-07 07:52:28 -0700

    6-prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,25 +2,33 @@
 /**
  *is_prime_number - returns 1 if input integer is a prime number
  * otherwise, 0
+ * is_prime_helper - 
  * @n: integer input
  * Return: 1 if integer
  * 0 if not
  */
-int is_prime_number(int n)
+int is_prime_helper(int n, int i)
 {
-	int i;
 
 	if (n <= 1)
 	{
 		return (0);
 	}
 
-	for (i = 2; i < n / 2; i++)
+	else if (i * i > n)
 	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
+		return (1);
 	}
-	return (1);
+	else if (n % i == 0)
+	{
+		return (0);
+	}
+	
+	return (is_prime_helper(n, i + 1));
+}
+
+int is_prime_number(int n)
+{
+	return (is_prime_helper(n, 2));
+	
 }
