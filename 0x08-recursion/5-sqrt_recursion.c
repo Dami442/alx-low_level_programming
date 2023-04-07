4f910b9d1dd912c41f4cdbbbbf2c1e23d90de941
Dami442 <damolowe442@gmail.com>
2023-04-07 06:08:30 -0700

    5-sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,37 +1,6 @@
 #include "main.h"
 /**
- *_sqrt_helper - checker
- * @n: number input
- *@start: lower bound of range of possible sqyare roots
- *@end: upper bound of range of possible square roots
- *Return: _sqrt_helper
- */
-int _sqrt_helper(int n, int start, int end)
-{
-	int mid  = (start + end) / 2;
-	int square = mid * mid;
-
-	if (start > end)
-	{
-		return (-1);
-	}
-
-	if (square == n)
-	{
-		return (mid);
-	}
-	else if (square < n)
-	{
-		return (_sqrt_helper(n, mid + 1, end));
-	}
-	else
-	{
-		return (_sqrt_helper(n, start, mid - 1));
-	}
-}
-
-/**
- * _sqrt_recursion - returns the natura
+ * _sqrt_recursion - returns the natural
  * @n: number input
  * Return: natural square root of n
  */
@@ -44,3 +13,34 @@ int _sqrt_recursion(int n)
 	}
 	return (_sqrt_helper(n, 0, n));
 }
+
+/**
+ *_sqrt_helper - checker
+ * @n: number input
+ *@start: lower bound of range of possible sqyare roots
+ *@end: upper bound of range of possible square roots
+ *Return: _sqrt_helper
+ */
+int _sqrt_helper(int n, int start, int end)
+{
+	int mid  = (start + end) / 2;
+
+	if (start > end)
+	{
+		return (-1);
+	}
+
+	if (mid * mid == n)
+	{
+		return (mid);
+	}
+	else if (mid * mid < n)
+	{
+		return (_sqrt_helper(n, mid + 1, end));
+	}
+	else
+	{
+		return (_sqrt_helper(n, start, mid - 1));
+	}
+}
+
