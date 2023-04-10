30161d2797444a62c0d6fe884d7472079476f245
Dami442 <damolowe442@gmail.com>
2023-04-10 15:31:07 -0700

    1-isdigit.c

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,9 +1,14 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
- * _isdigit - checks for digit
- * Return: 0
+ *_isdigit - checks for digit
+ *@c: parameter to be used
+ *Return: 1 if parameter is a digit
+ *and 0 if otherwise
  */
 int _isdigit(int c)
 {
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
