fda317201e90fe013ba87083b3568c3caf5fd631
Dami442 <damolowe442@gmail.com>
2023-04-12 14:34:37 -0700

    task-4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,9 +1,19 @@
+#include "main.h"
 #include <stdlib.h>
+/**
+ * free_grid - frees 2 dim grid
+ * @grid: grid par
+ * @height: height par
+ * Return: 0 on success
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
 
-void free_grid(int **grid, int height) {
-    for (int i = 0; i < height; i++) {
-        free(grid[i]);
-    }
-    free(grid);
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
 
