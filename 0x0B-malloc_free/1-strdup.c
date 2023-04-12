b04c31de66e3687d1df6358421dfc200f77de1ce
Dami442 <damolowe442@gmail.com>
2023-04-12 11:37:13 -0700

    task-1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,4 +1,31 @@
 #include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
 /**
- * @create_ar
- *
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ * @str: string par
+ * Return: Null and cop
+ */
+char *_strdup(char *str)
+{
+	size_t len = strlen(str) + 1;
+	char *cop = malloc(len);
+
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	if (cop == NULL)
+	{
+		return (NULL);
+	}
+
+	memcpy(cop, str, len);
+
+	return (cop);
+}
+
