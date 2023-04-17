101447c85b783fd659ae61e31e7dd4481959df59
Dami442 <damolowe442@gmail.com>
2023-04-17 07:22:48 -0700

    0-poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - data type for dog details
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: 0
+ */
+
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
+#endif
