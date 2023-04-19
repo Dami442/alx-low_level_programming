0c7d1d01fd84b49455706ffc259787becf4a0c05
Dami442 <damolowe442@gmail.com>
2023-04-19 10:34:43 -0700

    task-0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
 #include "function_pointers.h"
-
 /**
  * print_name - prints a name
  * @name: string of chars
- * @f: function pointer
- * Return: 0
+ * @f: pointer to function
+ * Return: 0 on success
  */
 void print_name(char *name, void (*f)(char *))
 {
