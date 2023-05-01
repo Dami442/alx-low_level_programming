fbbd03531f59d774a6f5564c47eac916091c3ae2
Dami442 <damolowe442@gmail.com>
2023-05-01 15:46:37 -0700

    task-5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list_t list
+ * @head: ptr to ptr that points to  head of list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
