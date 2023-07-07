e6e8885cd3ed831af69aa23d4dd466d62365ec32
Dami442 <damolowe442@gmail.com>
2023-07-07 11:52:36 -0700

    task-2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,6 +11,11 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int m =  1UL << index;
 
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
 	if ((n & m) != 0)
 	{
 		return (1);
