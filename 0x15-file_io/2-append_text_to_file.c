be7b9007271b3b870599b5d95d38876273bca08e
Dami442 <damolowe442@gmail.com>
2023-07-13 12:12:00 +0000

    task-2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,23 +19,36 @@ int append_text_to_file(const char *filename, char *text_content)
 
 
 	if (filename == NULL)
+	{
 		return (-1);
-	file = open(filename, O_RDWR | O_APPEND, 0600);
+	}
+	file = open(filename, O_RDONLY);
 	if (file == -1)
+	{
 		return (-1);
-	if (text_content != NULL)
-		close(file);
+	}
+	close(file);
+
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{	close(file);
+		return (-1);
+	}
+
 	while (text_content[bytes_to_write] != '\0')
 	{
 		bytes_to_write++;
 	}
 	write_bytes = write(file, text_content, bytes_to_write);
+	close(file);
 	if (write_bytes == -1)
 	{
-		close(file);
 		return (-1);
 	}
-	close(file);
 	return (1);
 }
 
